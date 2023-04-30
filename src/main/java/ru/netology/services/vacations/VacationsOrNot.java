19285487c23ea69465cb502201ba21eef8723c59
DS <mc_sd@bk.ru>
2023-04-30 11:17:44 -0300

    reformat code

--- src/main/java/ru/netology/services/vacations/VacationsOrNot.java
+++ src/main/java/ru/netology/services/vacations/VacationsOrNot.java
@@ -1,4 +1,5 @@
 package ru.netology.services.vacations;
+
 public class VacationsOrNot {
     public int calculate(int income, int expenses, int threshold) {
         int count = 0;
@@ -6,8 +7,8 @@ public class VacationsOrNot {
         for (int i = 0; i < 12; i++) {
             if (money >= threshold) {
                 count++;
-                money = money- expenses;
-                money = money/3;
+                money = money - expenses;
+                money = money / 3;
             } else {
                 money = money + income - expenses;
             }
@@ -17,8 +18,5 @@ public class VacationsOrNot {
     }
 
 
-
-
-
-    }
+}
 
